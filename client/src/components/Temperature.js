be2174a5b05076b4e6c0bb152810a5f365418799
Friedger Müffke <mail@friedger.de>
2020-03-19 18:16:05 +0100

    fix temperature

--- client/src/components/Temperature.js
+++ client/src/components/Temperature.js
@@ -3,19 +3,18 @@ import { Card } from 'react-bootstrap';
 import '../css/themePalette.css';
 import '../css/Temperature.css';
 
-export default Temperature = ({ healthRecord, allRecords }) => {
-  const averageTemperature =
-    allRecords.reduce(record => {
-      record.temperature;
-    }) / allRecords.length;
+export default ({ healthRecord, allRecords }) => {
+  const averageTemperature = allRecords
+    ? allRecords.reduce((record, sum) => {
+        return record.temperature + sum;
+      }, 0) / allRecords.length
+    : 'no data';
   return (
     <Card>
       <Card.Body>
-        <Card.Title id="Temperature-temp">{healthRecord.temperature}F</Card.Title>
+        <Card.Title id="Temperature-temp">{healthRecord ? healthRecord.temperature : '??'}F</Card.Title>
         <Card.Subtitle id="Temperature-avgTempText">{averageTemperature}</Card.Subtitle>
       </Card.Body>
     </Card>
   );
 };
-
-export default Temperature;
