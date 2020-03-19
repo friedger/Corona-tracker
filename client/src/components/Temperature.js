c97ca57f2e717a9acff951c94226b80f47b22b30
Friedger Müffke <mail@friedger.de>
2020-03-19 12:28:02 +0100

    add user data for temperature

--- client/src/components/Temperature.js
+++ client/src/components/Temperature.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import { Card }  from 'react-bootstrap';
-import "../css/themePalette.css";
-import "../css/Temperature.css";
+import { Card } from 'react-bootstrap';
+import '../css/themePalette.css';
+import '../css/Temperature.css';
 
-class Temperature extends Component {
-  render () {
-    return (
-      <Card>
-        <Card.Body>
-            <Card.Title id="Temperature-temp">
-              98.8F
-            </Card.Title>
-            <Card.Subtitle id="Temperature-avgTempText">
-              Average Temp
-            </Card.Subtitle>
-        </Card.Body>
-      </Card>
-    )
-  }
-}
+export default Temperature = ({ healthRecord, allRecords }) => {
+  const averageTemperature =
+    allRecords.reduce(record => {
+      record.temperature;
+    }) / allRecords.length;
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title id="Temperature-temp">{healthRecord.temperature}F</Card.Title>
+        <Card.Subtitle id="Temperature-avgTempText">{averageTemperature}</Card.Subtitle>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Temperature;
